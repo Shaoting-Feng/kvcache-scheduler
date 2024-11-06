81a754c6225275bc2bd7616ba77e269bfa3364ba
REDACTED <eaminchan@nature.cs.uchicago.edu>
2024-11-06 03:19:52 -0600

    send naive strat

--- flowsim/__init__.py
+++ flowsim/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["Buffer", "Receiver", "Sender"]
 
-from buffer import Buffer
-from receiver import Receiver
-from sender import Sender
+from .buffer import Buffer
+from .receiver import Receiver
+from .sender import Sender
